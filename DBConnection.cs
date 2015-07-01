00c31cd2a11b09105cd126bbdfde71dc3a3b101d
Alina Garaeva <crazyalin91@mail.ru>
2015-07-01 19:36:03 +0300

    Parallel SQL Queries
    Parallel SQL Queries using TPL in C#

---
+++ DBConnection.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ParallelSQLQueries
+{
+    class DBConnection
+    {
+      
+
+        public static SqlConnection  GetConnectionString()
+        {
+            //set connection string with db
+            return new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=F:\Алина\Education\МАГИСТРАТУРА\Магистратура\2 term\Parallel Programming\Projects\CourseWork\ParallelSQLQueries\ParallelSQLQueries\DATABASE.mdf;Integrated Security=True");
+        }
+//--------------------Выборка всех книг--------------------------------------//
+        public static SqlDataReader SelectAllBooks()
+        {
+            Console.WriteLine("Run Querie#1 Time:{0}\n", DateTime.Now.TimeOfDay);   
+            SqlConnection DBConn = GetConnectionString();
+            DBConn.Open(); //open db
+            //sql command
+            SqlCommand command = new SqlCommand("exec SelectAllBooks", DBConn);
+            SqlDataReader reader = null;
+            try
+            {
+                //execute query
+                reader = command.ExecuteReader();
+                Console.WriteLine("Querie#1 was succesful. | Time:{0}\n", DateTime.Now.TimeOfDay);
+                
+            }
+            catch
+            {
+                Console.WriteLine("Error! Query#1!\n");
+
+            }
+            return reader;//return extracted data
+          
+        }
+//--------------------Выборка всех студентов--------------------------------------//
+        public static SqlDataReader SelectAllStudents()
+        {
+            Console.WriteLine("Run Querie#2 Time:{0}\n", DateTime.Now.TimeOfDay);
+            SqlConnection DBConn = GetConnectionString();
+            DBConn.Open();
+
+            SqlCommand command = new SqlCommand("exec SelectAllStudents", DBConn);
+            SqlDataReader reader = null;
+            try
+            {
+                reader=command.ExecuteReader();
+                Console.WriteLine("Querie#2 was succesful. | Time:{0}\n", DateTime.Now.TimeOfDay);
+                
+            }
+            catch
+            {
+                Console.WriteLine("Error! Query#2!\n");
+
+            }
+            return reader;
+        }
+    
+ //--------------------Выборка всех студентов и книг-------------------------//
+        public static SqlDataReader  SelectAllStudentsKeepBook()
+        {
+            Console.WriteLine("Run Querie#3 Time:{0}\n" , DateTime.Now.TimeOfDay);
+            SqlConnection DBConn = GetConnectionString();
+            DBConn.Open();
+        
+            SqlCommand command = new SqlCommand("exec SelectAllStudentsBook", DBConn);
+            
+            SqlDataReader reader = null;
+            try
+            {
+                reader=command.ExecuteReader();
+                Console.WriteLine("Querie#3 was succesful. | Time:{0}\n", DateTime.Now.TimeOfDay);
+               
+                
+            }
+            catch
+            {
+                Console.WriteLine("Error! Query#3!\n");
+               
+            }
+            return reader;
+            
+           
+           
+           // return m;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data;
+using System.Diagnostics;
+
+namespace ParallelSQLQueries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+//------------------------Serial Queries-----------------------------//
+
+           Stopwatch watch = new Stopwatch();
+          
+            /*
+           watch.Reset();
+           watch.Start();
+
+           DBConnection.SelectAllBooks();
+           DBConnection.SelectAllStudents();
+           DBConnection.SelectAllStudentsKeepBook();
+
+           watch.Stop();
+
+
+           Console.WriteLine("\nSerial queries finished with time:{0} seconds\n\n", watch.Elapsed.TotalSeconds);
+            
+            */
+            
+
+
+//-------------------------Parallel Queries------------------------------//            
+            //TaskFactory tasks=new TaskFactory();
+            
+            List<Task> tasks = new List<Task>();//create lisyof tasks
+            watch.Reset();
+            watch.Start();//start timer
+
+            //execute first task
+            tasks.Add(Task.Factory.StartNew(() =>
+            {
+                
+             DBConnection.SelectAllBooks();
+                
+            }));
+            
+
+            
+            tasks.Add(Task.Factory.StartNew(() =>
+            {
+
+             DBConnection.SelectAllStudents();
+               
+            }));
+            
+
+           
+            tasks.Add(Task.Factory.StartNew(() =>
+            {
+
+                DBConnection.SelectAllStudentsKeepBook();
+                
+                
+            }));
+
+            //waiting untill all task was executed
+            Task.WaitAll(tasks.ToArray());
+            watch.Stop();
+               
+            Console.WriteLine("\nParallel queries finished with time:{0} seconds\n", watch.Elapsed.TotalSeconds);
+            
+            Console.ReadKey();
+
+          
+
+        }
+    }
+}
